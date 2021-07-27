ca1675db151d505aa330599156a04641d213bd13
prateek-ml <prateekb1912@gmail.com>
2021-07-27 09:34:02 +0530

    Bind data to the View holder in the recycler view

--- app/src/main/java/com/criclytica/reademall/BookCategoryRVAdapter.kt
+++ app/src/main/java/com/criclytica/reademall/BookCategoryRVAdapter.kt
@@ -1,18 +1,35 @@
 package com.criclytica.reademall
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.security.AccessController.getContext
 
 val bookCategories = arrayOf("2022 Reading List", "Non-Fiction", "Fiction", "Self-Help")
 
-class BookCategoryRVAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class BookCategoryRVAdapter : RecyclerView.Adapter<BookCategoryRVAdapter.CategorySelectViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+
+    class CategorySelectViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val listPosition = itemView.findViewById(R.id.tvItemNum) as TextView
+        val listTitle = itemView.findViewById(R.id.tvCategoryTitle) as TextView
+
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategorySelectViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.category_select_view_holder,
+                parent,
+                false)
+
+        return CategorySelectViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CategorySelectViewHolder, position: Int) {
+        holder.listPosition.text = "${position + 1}."
+        holder.listTitle.text = bookCategories[position]
     }
 
     override fun getItemCount(): Int = bookCategories.size
