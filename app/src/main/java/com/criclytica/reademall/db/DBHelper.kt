9dd31a279e2f739aca281d84909cca5a52d427a1
prateek-ml <prateekb1912@gmail.com>
2021-08-08 08:49:34 +0530

    Create a model for the database

--- app/src/main/java/com/criclytica/reademall/db/DBHelper.kt
+++ app/src/main/java/com/criclytica/reademall/db/DBHelper.kt
@@ -1,5 +1,20 @@
 package com.criclytica.reademall.db
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DBHelper: SQLiteOpenHelper
+class DBHelper(context: Context): SQLiteOpenHelper(
+        context,
+        "booklists.db",
+        null,
+        1
+) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+}
